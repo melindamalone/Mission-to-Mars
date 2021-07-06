56923e1c6ba3a095a57a6a6f7146a0f56d6e5698
Melinda <mrs.melindamalone@gmail.com>
2021-07-06 14:31:38 -0500

    feat: add ipynb and py files

--- scraping.py
+++ scraping.py
@@ -6,66 +6,69 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 import pandas as pd
 
-# Set up Splinter
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+# Set up function
+def scrape_all():
 
-#Visit the mars nasa news site
-url = 'https://redplanetscience.com'
-browser.visit(url)
+    # Set up Splinter
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
 
-#Optional delay for loading the page
-browser.is_element_present_by_css('div.list_text', wait_time=1)
+    #Visit the mars nasa news site
+    url = 'https://redplanetscience.com'
+    browser.visit(url)
 
-#Convert the browser html to a soup object and then quit the browser
-html = browser.html
-news_soup = soup(html, 'html.parser')
+    #Optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
 
-slide_elem = news_soup.select_one('div.list_text')
+    #Convert the browser html to a soup object and then quit the browser
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
 
-slide_elem.find('div', class_='content_title')
+    slide_elem = news_soup.select_one('div.list_text')
 
-# Use the parent element to find the first `a` tag and save it as `news_title`
-news_title = slide_elem.find('div', class_='content_title').get_text()
-news_title
+    slide_elem.find('div', class_='content_title')
 
-# Use the parent element to find the paragraph text
-news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-news_p
+    # Use the parent element to find the first `a` tag and save it as `news_title`
+    news_title = slide_elem.find('div', class_='content_title').get_text()
+    news_title
 
-### JPL Space Images Featured Image
+    # Use the parent element to find the paragraph text
+    news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+    news_p
 
-#Visit URL
-url = 'https://spaceimages-mars.com'
-browser.visit(url)
+    ### JPL Space Images Featured Image
 
-# Find and click the full image button 
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
+    #Visit URL
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
 
-# Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
+    # Find and click the full image button 
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
 
-# Find the relative image url
-img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-img_url_rel
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
 
-# Use the base URL to create an absolute URL
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-img_url
+    # Find the relative image url
+    img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+    img_url_rel
 
-### Mars Facts
-df = pd.read_html('https://galaxyfacts-mars.com')[0]
-df.head()
+    # Use the base URL to create an absolute URL
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+    img_url
 
-df.columns=['description', 'Mars', 'Earth']
-df.set_index('description', inplace=True)
-df
+    ### Mars Facts
+    df = pd.read_html('https://galaxyfacts-mars.com')[0]
+    df.head()
 
-df.to_html()
+    df.columns=['description', 'Mars', 'Earth']
+    df.set_index('description', inplace=True)
+    df
 
-browser.quit()
+    df.to_html()
+
+    browser.quit()
 
 
 
